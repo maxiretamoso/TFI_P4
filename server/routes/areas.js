ab793b1127f18cbffaff6c6cbb07a43163a55f3d
Maira <mairagabrielafrancia@gmail.com>
2026-09-23 19:18:28 -0400

    comentarios

--- server/routes/areas.js
+++ server/routes/areas.js
@@ -5,6 +5,16 @@ const verificarToken = require("../middlewares/verificarToken");
 const verificarRol = require("../middlewares/verificarRol");
 const { body, validationResult } = require("express-validator");
 
+
+
+/**
+ * TODA ESTA LOGICA DEBE IR DENTRO DE SERVICES, NO EN ROUTES. S
+ * LOS CONTROLLERS LLAMAN AL SERVICE, Y EL SERVICE LLAMA A LA DB.
+ * ROUTES SOLO DEBEN LLAMAR A LOS CONTROLLERS, NO A LA DB DIRECTAMENTE.
+ * LOS ERRORES DE RUTAS SE ESTAN IMPRIMIENDO EN CONSOLA DE SERVIDOR , ESTO NO DEBE SER ASI, DEBE HABER UN MIDDLEWARE DE ERRORES 
+ * QUE LOS MANEJE Y LOS IMPRIMA EN CONSOLA DEL NAVEGADOR NO EN EL SERVIDOR.
+ */
+
 router.get("/", async (req, res, next) => {
   try {
     const r = await pool.query("SELECT * FROM areas WHERE activo=1 ORDER BY descripcion");
